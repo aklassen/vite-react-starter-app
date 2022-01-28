805a726a41e7ea0e9119a5c9a2c0aa4f438dfe43
André Klaßen <andre.klassen@gmail.com>
2022-01-28 15:06:29 +0100

    setup eslint and prettier

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  preset: "vite-jest",
+  preset: 'vite-jest',
 
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testMatch: [
-    "<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}",
-    "<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}",
+    '<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}',
+    '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}',
   ],
-  testEnvironment: "jest-environment-jsdom",
+  testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
-    "\\.(css|sass|scss)$": "identity-obj-proxy",
+    '\\.(css|sass|scss)$': 'identity-obj-proxy',
   },
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({
-    fastRefresh: process.env.NODE_ENV !== 'test'
-  })],
+  plugins: [
+    react({
+      fastRefresh: process.env.NODE_ENV !== 'test',
+    }),
+  ],
   css: {
     modules: {
       localsConvention: 'camelCaseOnly',
